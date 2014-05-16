c4577cdea8b80fa091419bea514949f245ad9907
Conrad Fay <conrad.p.fay@gmail.com>
2014-05-16 08:54:25 -0700

    Added cloud movement.

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloud : MonoBehaviour {
+
+    public float speed = 1.0f;
+    public float startPosX;
+    public float endPosX;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+
+        if (transform.position.x > endPosX)
+            transform.position = new Vector3(startPosX, transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/CloudSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudSpawner : MonoBehaviour {
+
+    public float density;
+    public float lowerDistanceBound = 0.5f;
+    public float upperDistanceBound = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
